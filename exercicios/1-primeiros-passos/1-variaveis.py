032e2350ccaaa09e23690c97488c19c40f293a9e
Sulivan Souza <sulivansouza@hotmail.com>
2025-03-27 13:39:18 +0000

    aprendervariaveis

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,16 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
+nome = 'Sulivan'
+sexo = 'Masculino'
+cor = 'Preta'
+estilomusica = 'R&B'
 
 # Declare variáveis de cada tipo numérico
-
+idade = 46
+salario = 5.300
 
 # Declare uma variável do tipo booleano
+casado = True
+solteiro = False
+
