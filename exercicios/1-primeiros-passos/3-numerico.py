d5f6ef90d612e05b7534ccc5a71f19f0d7be9e45
Sulivan Souza <sulivansouza@hotmail.com>
2025-03-27 15:17:39 +0000

    numerosestudos

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -1,11 +1,16 @@
 data_nascimento = '05-10-1976'
 idade_numerica = 46
 altura = 1.74
-
+divisao = 9/2
 # Descubra o tipo de dado de cada variável acima
 print(type(data_nascimento))
 print(type(altura))
 print(type(idade_numerica))
+print(divisao)
+
+decimal = 30/8
+print(decimal)
+round(decimal,1)
 
 # Realize uma operação entre dados do tipo string e inteiro
 
