119a50afdc47331645a7194961109146e8742133
Sulivan Souza <sulivansouza@hotmail.com>
2025-03-27 14:56:24 +0000

    tiponumerico

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -3,7 +3,9 @@ idade_numerica = 46
 altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
-
+print(type(data_nascimento))
+print(type(altura))
+print(type(idade_numerica))
 
 # Realize uma operação entre dados do tipo string e inteiro
 
